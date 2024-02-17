4252c3cbda8194a6b013513decfe8cbdfa610b09
Basuraj <basurajp135@gmail.com>
2024-02-17 10:57:51 +0530

    jwt and bcrpt implemented

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,11 +53,37 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.ispasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
+userSchema.methods.ispasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-}
+// jwt  is beared token means , is just like key
 
+userSchema.methods.generateAcessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SCRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SCERET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
