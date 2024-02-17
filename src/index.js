cbc1e68c51afd9f29dbe7ddc6b41dc484fa11028
Basuraj <basurajp135@gmail.com>
2024-02-17 06:52:48 +0530

    utils code api error and api response  class done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-dotenv.config({path:'./env'});
+dotenv.config({ path: "./env" });
 
-
-connectDB();
+connectDB()
+  .then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`sever is running is runnig on ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => console.log("MongoDb connection failed  !!! ", err));
