38abf4ce51a128c1babdcb166459b886b0ef5262
Basuraj <basurajp135@gmail.com>
2024-02-17 15:20:58 +0530

    multer setup done

---
+++ src/utils/cloundary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// we are using unlink for deleting the file from the server
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDNIARY_CLOUD_NAME,
+  api_key: process.env.CLOUDNIARY_API_KEY,
+  api_secret: process.env.CLOUDNIARY_API_SCERET,
+});
+
+const uploadOnCloudnry = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file is uploaded on cloudinary
+    console.log("file is uploaded on cloudinary ", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //  removed the locally saved temporary file  as the upload operation got failed
+    return null
+  }
+};
+
+
+
+export { uploadOnCloudnry };
